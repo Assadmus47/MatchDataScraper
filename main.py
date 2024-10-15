e1df9da7bcbd175754dc01a8fdefdd79cf3d67b3
assadMus47 <assadkac47@gmail.com>
2024-10-15 01:07:24 +0100

    feat(main): csv
    append all the match info on a dictionaire and create a csv files and adding info to it
    
    BREAKING CHANGE:

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main(page=page):
     championships = soup.find_all('div', {'class': 'matchCard'})
     
     def get_matche_info(championship):
-        # make sure that contetns is not empty
+        # cheking if contetns is not empty
         if championship.contents:
             championship_title = championship.contents[1].find("h2").text.strip()
             all_matches = championship.contents[3].find_all('div', {'class': 'item finish liItem'})
@@ -26,17 +26,28 @@ def main(page=page):
                 
                 #get match score 
                 score = all_matches[i].find('div', {'class': 'MResult'}).find_all('span',{'class':'score'})
-                score = f"{score[0].text.strip()} - {score[1].text.strip()}"
+                score = f"{score[0].text.strip()}-{score[1].text.strip()}"
                 
                 #get match time
-                time = all_matches[i].find('div', {'class': 'MResult'}).find_all('span',{'class':'time'}).text.strip()
+                time = all_matches[i].find('div', {'class': 'MResult'}).find('span',{'class':'time'}).text.strip()
                 
-                
-                
+                # add match info to match details 
+                matches_details.append({"نوع البطولة":championship_title,"وقت المبارات":time,"فريق الاول":team_a,"فريق الثاني":team_b,"النتيجة":score})
+    
+    # cheking if contetns is not empty            
     if championships:
-        get_matche_info(championships[0])
+        for i in range(len(championships)):
+            get_matche_info(championships[i])
     else:
         print("No championships found")
+        
+    keys = matches_details[0].keys()
+    #adding match info to a csv file 
+    with open ('matches.csv','w', encoding='utf-8', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(matches_details)
+        print ("file created ")
 
 if __name__ == "__main__":
     main(page) 
